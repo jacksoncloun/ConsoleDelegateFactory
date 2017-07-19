3a7c5c9959969acf93e06d5290634602eddfdb09
270669482@qq.com <270669482@qq.com>
2017-07-19 10:35:16 +0800

    默认信息

--- ConsoleApplication1/Abstract和Interface/Class1.cs
+++ ConsoleApplication1/Abstract和Interface/Class1.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Abstract和Interface
 {
     /// <summary>
+    /// 普通类
     /// 这里讲述抽象类和接口
     /// 一个普通类如果继承了接口，那么必须实现接口里面的所有方法的方法体
     /// 一个普通类如果派生自某个抽象类，那么抽象类里面的方法可以直接调用，而不用继承所有方法
@@ -28,10 +29,11 @@ namespace Abstract和Interface
     }
 
     /// <summary>
-    ///  只要是抽象属性，不管是抽象类还是抽象方法都要加上 abstract关键字，如果抽象类没有加，但是在抽象方法中加了，那么在编译的时候默认这个类就会给class加上abstract关键字变成抽象类
-    ///  普通类实现接口的必须要实现接口里面的方法，抽象类实现接口不用实现接口里面的方法体
+    /// 抽象类
+    /// 只要是抽象属性，不管是抽象类还是抽象方法都要加上 abstract关键字，如果抽象类没有加，但是在抽象方法中加了，那么在编译的时候默认这个类就会给class加上abstract关键字变成抽象类
+    /// 普通类实现接口的必须要实现接口里面的方法，抽象类实现接口不用实现接口里面的方法体
     /// </summary>
-    public abstract class AbstractClass : IClassInterface   
+    public abstract class AbstractClass : IClassInterface
     {
         public abstract void method3()
         { }
